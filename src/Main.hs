b4fe7001e333ccd42f39de881c7dd33f5823aadf
MacKinley Smith <smit1625@msu.edu>
2017-03-25 16:45:16 -0600

    Use camel-case

--- src/Main.hs
+++ src/Main.hs
@@ -21,15 +21,15 @@ config = Config
     <*> some ( argument str $ metavar "FILES OR DIRECTORIES..." )
 
 main :: IO ()
-main = transcode_each =<< execParser opts
+main = transcodeEach =<< execParser opts
   where
     opts = info ( config <**> helper)
       ( fullDesc
      <> progDesc "Encode video files for usage with the PlayStation VR through LittlStar."
      <> header "psvr_transcode - a simple FFmpeg-based utility to encode video for PSVR" )
 
-transcode_each :: Config -> IO ()
-transcode_each (Config _ _ []) = return ()
-transcode_each (Config quiet' monitor' (path:paths)) = do
+transcodeEach :: Config -> IO ()
+transcodeEach (Config _ _ []) = return ()
+transcodeEach (Config quiet' monitor' (path:paths)) = do
   transcode path
-  transcode_each $ Config quiet' monitor' paths
+  transcodeEach $ Config quiet' monitor' paths
