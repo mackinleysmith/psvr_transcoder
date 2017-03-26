e922a08192062cd0c17fc22bbe0c8c3106f306ef
MacKinley Smith <smit1625@msu.edu>
2017-03-25 19:48:06 -0600

    How is this even working?

--- src/PsvrTranscoder.hs
+++ src/PsvrTranscoder.hs
@@ -8,18 +8,17 @@ import           System.Exit
 import           System.Process
 
 transcode :: String -> IO Bool
-transcode path_str = do
-  let input_file = decodeString path_str
-  let (maybe_working_base, maybe_working_ext) = splitExtension input_file
-
+transcode path_str =
   case toText maybe_working_base of
     Right working_base -> runFFmpegOn working_base input_file maybe_working_ext
     _ -> return False
+  where
+    input_file = decodeString path_str
+    (maybe_working_base, maybe_working_ext) = splitExtension input_file
 
 runFFmpegOn :: T.Text -> Filesystem.Path.CurrentOS.FilePath -> Maybe T.Text -> IO Bool
-runFFmpegOn working_base input_file maybe_working_ext = do
-  exit_code <- system $ command working_base maybe_working_ext input_file
-  return $ exit_code == ExitSuccess
+runFFmpegOn working_base input_file maybe_working_ext =
+  pure (== ExitSuccess) <*> system ( command working_base maybe_working_ext input_file )
 
 command :: T.Text -> Maybe T.Text -> Filesystem.Path.CurrentOS.FilePath -> String
 command working_base maybe_working_ext input_file =
@@ -28,6 +27,7 @@ command working_base maybe_working_ext input_file =
     options = "-vcodec libx264 -profile:v high -level:v 4.2 -s 1920x1080 -preset slow -crf 18 -c:a libfdk_aac -vbr 5"
     output_file = outputFileFor (decodeString $ T.unpack working_base) maybe_working_ext
 
+outputFileFor :: Filesystem.Path.CurrentOS.FilePath -> Maybe T.Text -> String
 outputFileFor working_base maybe_working_ext =
   encodeString working_base ++ "_psvr_180_sbs." ++ original_extension
   where
