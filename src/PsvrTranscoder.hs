ff6e18293956db8fe0a8bd0ee17be9e6edbc4fc9
MacKinley Smith <smit1625@msu.edu>
2017-03-25 19:35:19 -0600

    Refactoring

--- src/PsvrTranscoder.hs
+++ src/PsvrTranscoder.hs
@@ -9,23 +9,26 @@ import           System.Process
 
 transcode :: String -> IO Bool
 transcode path_str = do
-  let maybe_input_file = decodeString path_str
-  case toText maybe_input_file of
-    Left _ -> return False
-    Right input_file -> do
-      let (maybe_working_base, maybe_working_ext) = splitExtension maybe_input_file
+  let input_file = decodeString path_str
+  let (maybe_working_base, maybe_working_ext) = splitExtension input_file
 
-      case toText maybe_working_base of
-        Right working_base -> thing working_base maybe_input_file maybe_working_ext
-        _ -> return False
+  case toText maybe_working_base of
+    Right working_base -> runFFmpegOn working_base input_file maybe_working_ext
+    _ -> return False
 
-thing :: T.Text -> Filesystem.Path.CurrentOS.FilePath -> Maybe T.Text -> IO Bool
-thing working_base input_file maybe_working_ext = do
+runFFmpegOn :: T.Text -> Filesystem.Path.CurrentOS.FilePath -> Maybe T.Text -> IO Bool
+runFFmpegOn working_base input_file maybe_working_ext = do
   exit_code <- system $ command working_base maybe_working_ext input_file
   return $ exit_code == ExitSuccess
 
 command :: T.Text -> Maybe T.Text -> Filesystem.Path.CurrentOS.FilePath -> String
 command working_base maybe_working_ext input_file =
-  "ffmpeg -i \"" ++ encodeString input_file ++ "\" -vcodec libx264 -profile:v high -level:v 4.2 -s 1920x1080 -preset slow -crf 18 -c:a libfdk_aac -vbr 5 \"" ++ output_file ++ "\""
+  "ffmpeg -i \"" ++ encodeString input_file ++ "\" " ++ options ++ " \"" ++ output_file ++ "\""
   where
-    output_file = T.unpack $ working_base `T.append` T.pack "_psvr_180_sbs" `T.append` T.pack "." `T.append` ( case maybe_working_ext of Just ext -> ext; _ -> T.pack "" )
+    options = "-vcodec libx264 -profile:v high -level:v 4.2 -s 1920x1080 -preset slow -crf 18 -c:a libfdk_aac -vbr 5"
+    output_file = outputFileFor (decodeString $ T.unpack working_base) maybe_working_ext
+
+outputFileFor working_base maybe_working_ext =
+  encodeString working_base ++ "_psvr_180_sbs." ++ original_extension
+  where
+    original_extension = case maybe_working_ext of Just ext -> T.unpack ext; _ -> ""
