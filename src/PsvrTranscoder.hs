e8fc61a8994c2650343f07454ce5786bda0495b1
MacKinley Smith <smit1625@msu.edu>
2017-03-26 16:30:51 -0600

    Rename some stuff for clarity

--- src/PsvrTranscoder.hs
+++ src/PsvrTranscoder.hs
@@ -8,40 +8,43 @@ import           System.Exit
 import           System.Process
 
 data VideoFile = VideoFile
-  { pathStr         :: String
-  , workingBase     :: T.Text
-  , maybeWorkingExt :: Maybe T.Text
-  , inputFile       :: Filesystem.Path.CurrentOS.FilePath }
+  { pathStr    :: String
+  , filePath   :: Filesystem.Path.CurrentOS.FilePath
+  , baseName   :: T.Text
+  , fileExt :: Maybe T.Text }
 
 transcode :: String -> IO ()
 transcode path_str =
   case toText maybe_working_base of
-    Right working_base -> do
+    Right base_name -> do
       _ <- runFFmpegOn
         VideoFile
           { pathStr = path_str
-          , workingBase = working_base
-          , maybeWorkingExt = maybe_working_ext
-          , inputFile = input_file }
+          , filePath = input_file
+          , baseName = base_name
+          , fileExt = maybe_ext }
       return ()
     _ -> return ()
   where
     input_file = decodeString path_str
-    (maybe_working_base, maybe_working_ext) = splitExtension input_file
+    (maybe_working_base, maybe_ext) = splitExtension input_file
 
 runFFmpegOn :: VideoFile -> IO Bool
 runFFmpegOn video_file = (== ExitSuccess) <$> system ( commandFor video_file )
 
 commandFor :: VideoFile -> String
-commandFor video_file @ VideoFile { pathStr = input_path } =
-  "ffmpeg -i \"" ++ input_path ++ "\" " ++ options ++ " \"" ++ output_file ++ "\""
+commandFor video_file @ VideoFile { pathStr = path_str } =
+  "ffmpeg -i \"" ++ path_str ++ "\" " ++ options ++ " \"" ++ output_file ++ "\""
   where
     options = "-vcodec libx264 -profile:v high -level:v 4.2 -s 1920x1080 -preset slow -crf 18 -c:a libfdk_aac -vbr 5"
     output_file = outputFileFor video_file
 
 outputFileFor :: VideoFile -> String
-outputFileFor VideoFile { workingBase = working_base, maybeWorkingExt = maybe_working_ext } =
+outputFileFor VideoFile { baseName = working_base, fileExt = maybe_ext } =
   filename ++ "_psvr_180_sbs." ++ original_extension
   where
     filename = T.unpack working_base
-    original_extension = case maybe_working_ext of Just ext -> T.unpack ext; _ -> ""
+    original_extension =
+      case maybe_ext of
+        Just ext -> T.unpack ext
+        _ -> ""
