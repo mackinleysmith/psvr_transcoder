fac96db58f66658769b35cd3cd384ad8933977b2
MacKinley Smith <smit1625@msu.edu>
2017-03-25 21:40:16 -0600

    Extract a VideoFile data type

--- src/PsvrTranscoder.hs
+++ src/PsvrTranscoder.hs
@@ -7,28 +7,38 @@ import           Filesystem.Path.CurrentOS
 import           System.Exit
 import           System.Process
 
+data VideoFile = VideoFile
+  { pathStr         :: String
+  , workingBase     :: T.Text
+  , maybeWorkingExt :: Maybe T.Text
+  , inputFile       :: Filesystem.Path.CurrentOS.FilePath }
+
 transcode :: String -> IO Bool
 transcode path_str =
   case toText maybe_working_base of
-    Right working_base -> runFFmpegOn working_base input_file maybe_working_ext
+    Right working_base ->
+      runFFmpegOn VideoFile
+        { pathStr = path_str
+        , workingBase = working_base
+        , maybeWorkingExt = maybe_working_ext
+        , inputFile = input_file }
     _ -> return False
   where
     input_file = decodeString path_str
     (maybe_working_base, maybe_working_ext) = splitExtension input_file
 
-runFFmpegOn :: T.Text -> Filesystem.Path.CurrentOS.FilePath -> Maybe T.Text -> IO Bool
-runFFmpegOn working_base input_file maybe_working_ext =
-  (== ExitSuccess) <$> system ( command working_base maybe_working_ext input_file )
+runFFmpegOn :: VideoFile -> IO Bool
+runFFmpegOn video_file = (== ExitSuccess) <$> system ( command video_file )
 
-command :: T.Text -> Maybe T.Text -> Filesystem.Path.CurrentOS.FilePath -> String
-command working_base maybe_working_ext input_file =
-  "ffmpeg -i \"" ++ encodeString input_file ++ "\" " ++ options ++ " \"" ++ output_file ++ "\""
+command :: VideoFile -> String
+command video_file @ (VideoFile input_path _ _ _) =
+  "ffmpeg -i \"" ++ input_path ++ "\" " ++ options ++ " \"" ++ output_file ++ "\""
   where
     options = "-vcodec libx264 -profile:v high -level:v 4.2 -s 1920x1080 -preset slow -crf 18 -c:a libfdk_aac -vbr 5"
-    output_file = outputFileFor (decodeString $ T.unpack working_base) maybe_working_ext
+    output_file = outputFileFor video_file
 
-outputFileFor :: Filesystem.Path.CurrentOS.FilePath -> Maybe T.Text -> String
-outputFileFor working_base maybe_working_ext =
-  encodeString working_base ++ "_psvr_180_sbs." ++ original_extension
+outputFileFor :: VideoFile -> String
+outputFileFor (VideoFile _ working_base maybe_working_ext _) =
+  T.unpack working_base ++ "_psvr_180_sbs." ++ original_extension
   where
     original_extension = case maybe_working_ext of Just ext -> T.unpack ext; _ -> ""
