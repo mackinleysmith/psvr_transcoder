df38f5a53dc8b7f49c8fd0dc1a57046ca6716a25
MacKinley Smith <smit1625@msu.edu>
2017-03-26 12:20:06 -0600

    Use named args, return IO ()

--- src/PsvrTranscoder.hs
+++ src/PsvrTranscoder.hs
@@ -13,16 +13,17 @@ data VideoFile = VideoFile
   , maybeWorkingExt :: Maybe T.Text
   , inputFile       :: Filesystem.Path.CurrentOS.FilePath }
 
-transcode :: String -> IO Bool
+transcode :: String -> IO ()
 transcode path_str =
   case toText maybe_working_base of
     Right working_base ->
-      runFFmpegOn VideoFile
-        { pathStr = path_str
-        , workingBase = working_base
-        , maybeWorkingExt = maybe_working_ext
-        , inputFile = input_file }
-    _ -> return False
+      runFFmpegOn
+        VideoFile
+          { pathStr = path_str
+          , workingBase = working_base
+          , maybeWorkingExt = maybe_working_ext
+          , inputFile = input_file } >> return ()
+    _ -> return ()
   where
     input_file = decodeString path_str
     (maybe_working_base, maybe_working_ext) = splitExtension input_file
@@ -31,14 +32,14 @@ runFFmpegOn :: VideoFile -> IO Bool
 runFFmpegOn video_file = (== ExitSuccess) <$> system ( command video_file )
 
 command :: VideoFile -> String
-command video_file @ (VideoFile input_path _ _ _) =
+command video_file @ VideoFile { pathStr = input_path } =
   "ffmpeg -i \"" ++ input_path ++ "\" " ++ options ++ " \"" ++ output_file ++ "\""
   where
     options = "-vcodec libx264 -profile:v high -level:v 4.2 -s 1920x1080 -preset slow -crf 18 -c:a libfdk_aac -vbr 5"
     output_file = outputFileFor video_file
 
 outputFileFor :: VideoFile -> String
-outputFileFor (VideoFile _ working_base maybe_working_ext _) =
+outputFileFor VideoFile { workingBase = working_base, maybeWorkingExt = maybe_working_ext } =
   filename ++ "_psvr_180_sbs." ++ original_extension
   where
     filename = T.unpack working_base
