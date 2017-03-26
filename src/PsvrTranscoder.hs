e214bd0f7d97e16b76a0fc2ba5d742c47267d4cd
MacKinley Smith <smit1625@msu.edu>
2017-03-25 19:25:55 -0600

    Extract some command methods

--- src/PsvrTranscoder.hs
+++ src/PsvrTranscoder.hs
@@ -16,9 +16,16 @@ transcode path_str = do
       let (maybe_working_base, maybe_working_ext) = splitExtension maybe_input_file
 
       case toText maybe_working_base of
-        Left _ -> return False
-        Right working_base -> do
-          let output_file = working_base `T.append` T.pack "_psvr_180_sbs" `T.append` T.pack "." `T.append` ( case maybe_working_ext of Just ext -> ext; _ -> T.pack "" )
-          let command = T.pack "ffmpeg -i \"" `T.append` input_file `T.append` T.pack "\" -vcodec libx264 -profile:v high -level:v 4.2 -s 1920x1080 -preset slow -crf 18 -c:a libfdk_aac -vbr 5 \"" `T.append` output_file `T.append` T.pack "\""
-          exit_code <- system $ T.unpack command
-          return $ exit_code == ExitSuccess
+        Right working_base -> thing working_base maybe_input_file maybe_working_ext
+        _ -> return False
+
+thing :: T.Text -> Filesystem.Path.CurrentOS.FilePath -> Maybe T.Text -> IO Bool
+thing working_base input_file maybe_working_ext = do
+  exit_code <- system $ command working_base maybe_working_ext input_file
+  return $ exit_code == ExitSuccess
+
+command :: T.Text -> Maybe T.Text -> Filesystem.Path.CurrentOS.FilePath -> String
+command working_base maybe_working_ext input_file =
+  "ffmpeg -i \"" ++ encodeString input_file ++ "\" -vcodec libx264 -profile:v high -level:v 4.2 -s 1920x1080 -preset slow -crf 18 -c:a libfdk_aac -vbr 5 \"" ++ output_file ++ "\""
+  where
+    output_file = T.unpack $ working_base `T.append` T.pack "_psvr_180_sbs" `T.append` T.pack "." `T.append` ( case maybe_working_ext of Just ext -> ext; _ -> T.pack "" )
